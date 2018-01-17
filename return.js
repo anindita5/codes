d3e86e8b884e551b16c59404b4bfc9d1043a3e3f
anindita5 <35261775+anindita5@users.noreply.github.com>
2018-01-17 18:27:52 +0530

    Add files via upload

---
+++ return.js
@@ -0,0 +1,89 @@
+function validate(){
+	
+	
+	var ticket =document.frm1.ticket.value;
+	
+	var price;
+	  
+	var Name=document.getElementById("name").value;
+	
+	var number=document.getElementById("no").value;
+	
+	
+      if (ticket == 1)
+    {
+        alert(Name + "\n" + number + "\n" + ticket  )
+        ticket.focus();
+		return true;
+    }
+	else if (ticket == 2)
+    {
+        alert(Name + "\n" + number + "\n" + ticket  )
+        ticket.focus();
+		return true;
+    }
+	else if (ticket == 3)
+    {
+        alert(Name + "\n" + number + "\n" + ticket  )
+        ticket.focus();
+		return true;
+    }
+	else if (ticket == 3)
+    {
+        alert(Name + "\n" + number + "\n" + ticket  )
+        ticket.focus();
+		return true;
+    }
+	else if (ticket == 4)
+    {
+        alert(Name + "\n" + number + "\n" + ticket  )
+        ticket.focus();
+		return true;
+    }
+	else if (ticket == 5)
+    {
+        alert(Name + "\n" + number + "\n" + ticket  )
+        ticket.focus();
+		return true;
+    }
+	else if (ticket == 6)
+    {
+        alert(Name + "\n" + number + "\n" + ticket  )
+        ticket.focus();
+		return true;
+    }
+	
+	else if (ticket == 7)
+    {
+        alert(Name + "\n" + number + "\n" + ticket  )
+        ticket.focus();
+		return true;
+    }
+	else if (ticket == 8)
+    {
+        alert(Name + "\n" + number + "\n" + ticket  )
+        ticket.focus();
+		return true;
+    }
+	else if (ticket == 9)
+    {
+        alert(Name + "\n" + number + "\n" + ticket  )
+        ticket.focus();
+		return true;
+    }
+	else if (ticket == 10)
+    {
+        alert(Name + "\n" + number + "\n" + ticket  )
+        ticket.focus();
+		return true;
+    }
+	
+	  return true;
+	  
+}
+	
+	
+
+	
+	
+
